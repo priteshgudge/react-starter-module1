81fda942a4d4e9787bbc4d39d589dfe419e395d9
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-21 11:56:58 +0530

    Routing on Click

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {getFunName} from '../helpers'
 
 class StorePicker extends React.Component {
+    storeNameInput = React.createRef()
+    goToStore = (event) =>{
+        event.preventDefault()
+        console.log(this.storeNameInput.current.value)
+        const storeValue = this.storeNameInput.current.value;
+        this.props.history.push(`/store/${storeValue}`)
+
+    }
+
+
     render() {
         return (
-          <form className='store-selector'>
+          <form className='store-selector' onClick={this.goToStore}>
               <h2> Please enter a store</h2>
-              <input type="text" required placeholder="Store Name"/>
+              <input type="text"
+                     required
+                     ref={this.storeNameInput}
+                     placeholder="Store Name"
+                     defaultValue={getFunName()}/>
               <button type="submit">Visit Store</button>
           </form>
         )
