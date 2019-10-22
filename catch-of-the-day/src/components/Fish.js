546af49902018d27612dcd2a9c8b14ef637b996f
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-22 13:15:58 +0530

    Some Styling

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,7 +3,7 @@ import {formatPrice} from '../helpers'
 
 const fish = (props) => {
     const fish = props.details;
-
+    const isAvailable = fish.status === "available";
     return (
 
         <li className="menu-fish">
@@ -12,7 +12,7 @@ const fish = (props) => {
                 <span className="price">{formatPrice(fish.price)}</span>
             </h3>
             <p>{fish.desc}</p>
-            <button>Add to Cart</button>
+            <button disabled={!isAvailable}>{isAvailable?"Add to Cart":"Sold Out"}</button>
 
 
         </li>
