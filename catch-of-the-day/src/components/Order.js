cab08fc17599b43c167fcd4b9cda8496b37d74f3
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-25 10:21:06 +0530

    Updating Order

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 class Order extends React.Component{
+
     render(){
+        const orderIds = Object.keys(this.props.order);
+        const orderTotal = orderIds.length >= 1 && orderIds.reduce((prevTotal,key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish && fish['status'] === 'available';
+            const price = fish['price'];
+            if (isAvailable){
+                return count * price + prevTotal;
+            }else{
+                return prevTotal;
+            }
+        })
+
         return(
-            <div className="order">
-                Order!!!
+            <div className="order-wrap">
+                <h2>Order </h2>
+                {orderTotal}
+                <p></p>
             </div>
         )
     }
